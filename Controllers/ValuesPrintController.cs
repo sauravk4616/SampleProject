d5925954608096826241a22140e890d14190261e
Saurav@4616 <sauravk4616@gmail.com>
2020-09-10 14:22:35 +0530

    New Controller Added

---
+++ Controllers/ValuesPrintController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesPrintController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getVal")]
+
+        public string getdetails()
+        {
+            return "My name Is Saurav";
+        }
+    }
+}
